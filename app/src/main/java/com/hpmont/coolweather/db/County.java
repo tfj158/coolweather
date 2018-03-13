6ad58abb54ff5e6b2241050bb312390c4dfc982c
hpmont001 <tianqinghe@hpmont.com>
2018-03-13 15:09:19 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/hpmont/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.hpmont.coolweather.db;
+
+//import DataSupport;
+import org.litepal.crud.DataSupport;
+/**
+ * Created by 田富杰 on 2018/3/13.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
